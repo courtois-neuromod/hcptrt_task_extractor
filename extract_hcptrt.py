522a0a3487a54f39b51ddd4cbd2132ec89632b88
arnaudbore <arnaud.bore@gmail.com>
2020-05-07 16:04:22 -0400

    refactor code + update config motor and emotion

--- extract_hcptrt.py
+++ extract_hcptrt.py
@@ -19,6 +19,11 @@ import os
 from convert_eprime.convert import _text_to_df
 import pandas as pd
 
+column = "column"
+value = "value"
+formula = "formula"
+when_no_value = "when_no_value"
+
 
 def _build_args_parser():
     p = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter,
@@ -66,22 +71,25 @@ def assert_task_df(df_columns, json_dict):
 
     for curr_event in json_dict["events"]:
         for curr_key in curr_event:
-            if "column" in curr_key:
-                if isinstance(curr_key["column"], list):
-                    allColumns = allColumns + curr_key["column"]
-                else:
-                    allColumns.append(curr_key["column"])
-
-            if isinstance(curr_key, list):
-                for listKey in curr_key:
-                    if isinstance(curr_key["column"], list):
-                        allColumns = allColumns + curr_key["column"]
+            if isinstance(curr_event[curr_key], list):
+                for element in curr_event[curr_key]:
+                    if column in element:
+                        if isinstance(element[column], list):
+                            allColumns = allColumns + element[column]
+                        else:
+                            allColumns.append(element[column])
+            else:
+                if column in curr_event[curr_key]:
+                    if isinstance(curr_event[curr_key][column], list):
+                        allColumns = allColumns + curr_event[curr_key][column]
                     else:
-                        allColumns.append(curr_key["column"])
+                        allColumns.append(curr_event[curr_key][column])
+
+    allColumns = list(dict.fromkeys(allColumns))
 
     for curr_column in allColumns:
-        if not set([curr_column]).issubset(df.columns):
-            logging.error("{} does not exist into df".format(curr_column))
+        if not set([curr_column]).issubset(df_columns):
+            raise IOError("Column: \"{}\" does not exist into df".format(curr_column))
 
 
 def get_ioi(df, ioi_dict):
@@ -102,18 +110,33 @@ def get_ioi(df, ioi_dict):
         Series of Index of interest.
     """
     indexes = np.zeros((len(df), ), dtype=bool)
-    for curr_condition in ioi_dict:
-        columnName = curr_condition["column"]
-        if isinstance(curr_condition["value"], list):
-            for columnValue in curr_condition["value"]:
+
+    columnName = ioi_dict[column]
+    if isinstance(columnName, list):  # Column(list)
+        if formula in ioi_dict:  # If formula
+            if ioi_dict[formula] == "intersection":
+                indexes = intersection_columns(df, ioi_dict)
+            else:
+                logging.error("function: get_ioi - If you have multiple columns you"
+                              "need a formula to know what should be "
+                              "done. Default merge")
+        else:
+            ioi_serie = merge_columns(df, ioi_dict)
+            indexes = ~np.isnan(ioi_serie.astype(np.float))
+
+    elif isinstance(columnName, str):  # Column(str)
+        if value in ioi_dict:  # Column(str) - Value
+            indexes = np.zeros((len(df),))
+            if isinstance(ioi_dict[value], str):  # Value(str)
+                columnValues = [ioi_dict[value]]
+            elif isinstance(ioi_dict[value], list):  # Value(list)
+                columnValues = ioi_dict[value]
+
+            for columnValue in columnValues:
                 curr_index = df[columnName] == columnValue
                 indexes = indexes | curr_index
-        elif curr_condition["value"] == 'notNAN':
-            curr_index = ~np.isnan(df[columnName].astype(np.float))
-            indexes = indexes | curr_index
-        else:
-            curr_index = df[columnName] == curr_condition["value"]
-            indexes = indexes | curr_index
+        else:  # Column(str) - No Value
+            indexes = df[columnName].astype(str) != "nan"
 
     return indexes
 
@@ -136,16 +159,16 @@ def get_onsets(df, onset_dict):
         Series of Index with onsets.
     """
 
-    if 'value' in onset_dict:
-        if onset_dict['value'] == 'merge' and isinstance(onset_dict['column'],
-                                                         list):
-            onset_serie = merge_columns(df, onset_dict['column'])
-        elif isinstance(onset_dict['column'], list):
-            logging.error('Use case {} not coded'.format(onset_dict['value']))
+    if value in onset_dict:
+        if onset_dict[value] == 'merge' and isinstance(onset_dict[column],
+                                                       list):
+            onset_serie = merge_columns(df, onset_dict)
+        elif isinstance(onset_dict[column], list):
+            logging.error('Use case {} not coded'.format(onset_dict[value]))
         else:
             logging.error('If you want to merge you need at least two columns')
     else:
-        onset_serie = merge_columns(df, [onset_dict['column']])
+        onset_serie = df[onset_dict[column]]
 
     onset_serie = onset_serie.rename('onset')
 
@@ -169,45 +192,118 @@ def get_durations(df, onset, duration_dict):
     duration_serie: pandas.core.series.Series
         Series of Index with durations.
     """
-    if 'formula' in duration_dict:
-        curr_formula = duration_dict['formula']
+    if formula in duration_dict:
+        curr_formula = duration_dict[formula]
         if len(curr_formula) == 3:
-            duration_serie = df[duration_dict['column']]
+            duration_serie = df[duration_dict[column]]
             duration_serie = duration_serie.shift(periods=curr_formula[0])
             if curr_formula[1] == 'subtract':
                 duration_serie = duration_serie.astype(np.float) - onset.astype(np.float)
             elif curr_formula[1] == 'add':
                 duration_serie = duration_serie.astype(np.float) + onset.astype(np.float)
         else:
-            logging.error('This is not coded yet')
-    elif isinstance(duration_dict['column'], str):
-        duration_serie = df[duration_dict['column']]
+            logging.error('get_duration - Formula is not coded yet')
+    elif isinstance(duration_dict[column], str):
+        duration_serie = df[duration_dict[column]]
+
+    if when_no_value in duration_dict:
+        if duration_dict[when_no_value] == 'median':
+            duration_serie[np.isnan(duration_serie.astype(np.float))] = np.nanmedian(duration_serie.astype(np.float))
+        elif duration_dict[when_no_value] == 'mean':
+            duration_serie[np.isnan(duration_serie.astype(np.float))] = np.nanmean(duration_serie.astype(np.float))
 
     duration_serie = duration_serie.rename('duration')
     return duration_serie
 
 
-def merge_columns(df, listColumns):
+def intersection_columns(df, curr_dict):
     """
-    Merge column inot Get Index of Interest from task['ioi'] list of dict.
+    Intersection of columns/values -> Get Index of Interest from event['ioi'].
 
     Parameters
     ----------
     df : pandas DataFrame
         Current DataFrame.
 
-    listColumns : list
-        List of column names to merge. You should not have overlap between
-        these columns.
+    curr_dict : dict
+        Dictionnary with column names and values.
+
+    Returns
+    -------
+    new_serie: pandas.core.series.Series
+        Indexes of intersection of columns/values.
+    """
+    listColumns = curr_dict[column]
+    listValues = curr_dict[value]
+
+    new_serie = df[listColumns[0]] == listValues[0]
+    listColumns = listColumns[1::]
+    listValues = listValues[1::]
+    for idx, curr_column in enumerate(listColumns):
+        new_serie2 = df[curr_column] == listValues[idx]
+        new_serie = new_serie & new_serie2
+
+    return new_serie
+
+
+def merge_columns(df, curr_dict):
+    """
+    Merge columns. Check if columns have the values with same indexes.
+
+    Parameters
+    ----------
+    df : pandas DataFrame
+        Current DataFrame.
+
+    curr_dict : dict
+        Dictionnary with column names and values.
 
     Returns
     -------
     new_serie: pandas.core.series.Series
         Merge serie.
     """
-    new_serie = pd.Series([])
-    for curr_column in listColumns:
-        new_serie = new_serie.combine_first(df[curr_column])
+    curr_columns = curr_dict[column]
+    if not isinstance(curr_columns, list):
+        raise IOError("If you want to merge columns you need at least "
+                      "2 columns.")
+
+    if value in curr_dict:  # Value exist
+        curr_values = curr_dict[value]
+        if isinstance(curr_values, list):  # Value (list)
+            if len(curr_values) != len(curr_columns):
+                raise IOError('List of values should match columns')
+        elif isinstance(curr_values, str):  # Value (str)
+            curr_values = [curr_values] * len(curr_columns)
+
+        listColumns = curr_columns[1::]
+        listValues = curr_values[1::]
+        index_serie = df[curr_columns[0]] == curr_values[0]
+        new_serie = df[curr_columns[0]]
+        new_serie[new_serie != curr_values[0]] = "nan"
+
+        for idx, curr_column in enumerate(listColumns):
+            new_index = df[curr_column == listValues[idx]]
+            new_serie = df[curr_column]
+            new_serie[new_serie != listValues[idx]] = "nan"
+            if np.any(new_index & index_serie):
+                raise IOError('Columns contain values at the same index')
+            else:
+                index_serie = index_serie | new_index
+                new_serie = new_serie.combine_first(new_serie)
+
+    else:  # Value does not exist
+        listColumns = curr_dict[column][1::]
+        new_serie = df[curr_dict[column][0]]
+        index_wo_nan = new_serie.astype(str) != "nan"
+
+        for curr_column in listColumns:
+            new_index = df[curr_column].astype(str) != "nan"
+            if np.any(new_index & index_wo_nan):
+                raise IOError('Columns contain values at the same index')
+            else:
+                index_wo_nan = index_wo_nan | new_index
+                new_serie = new_serie.combine_first(df[curr_column])
 
     return new_serie
 
@@ -233,21 +329,17 @@ def get_key(df, columnName, key_dict):
         Series from specific key.
     """
 
-    if isinstance(key_dict['column'], str):
-        key_serie = df[key_dict['column']]
+    if isinstance(key_dict[column], str):
+        key_serie = df[key_dict[column]]
         if key_dict['type'] == 'stim':
             key_serie = '../../stimulis/' + key_serie
         elif columnName == "nbloc":
             key_serie = key_serie.astype(np.float) - \
                             np.floor(key_serie.astype(np.float)/2)
-
-    elif isinstance(key_dict['column'], list):
-        if key_dict['value'] == 'merge':
-            key_serie = merge_columns(df, key_dict['column'])
-        else:
-            logging.error('not coded yet')
+    elif isinstance(key_dict[column], list):
+        key_serie = merge_columns(df, key_dict)
     else:
-        logging.error('not coded yet')
+        logging.error('not coded yet - get_key')
 
     key_serie = key_serie.rename(columnName)
 
